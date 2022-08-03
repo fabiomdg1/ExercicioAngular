6a18606e623823e057c8a496f44666cd399972e1
Fabio <fabiomdg1@gmail.com>
2022-08-03 15:57:00 -0300

    first commit

---
+++ src/app/interpolation/interpolation.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolation',
+  templateUrl: './interpolation.component.html',
+  styleUrls: ['./interpolation.component.css']
+})
+export class InterpolationComponent {
+  nome:String = "Fabio Mattos de Godoy";
+
+}
---
+++ src/app/propertybinding/propertybinding.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-propertybinding',
+  templateUrl: './propertybinding.component.html',
+  styleUrls: ['./propertybinding.component.css']
+})
+export class PropertybindingComponent {
+
+  urlCat:String = "https://loremflickr.com/320/240";
+
+  urlDog:String = "https://loremflickr.com/320/240/dogs";
+
+}
